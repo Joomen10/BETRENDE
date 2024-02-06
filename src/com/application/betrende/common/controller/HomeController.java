d61429a2520aca7883ede7acd9bdbb575ea135df
Christopher Park <park11871247@gmail.com>
2024-02-06 10:48:02 +0900

    Add files via upload

---
+++ src/com/application/betrende/common/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.application.betrende;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HomeController {
+	
+
+//	@GetMapping("/")
+//	public String home() {
+//		System.out.println("aaa");
+//		return "/main";
+//	}
+//	
+//	@GetMapping("/selectuserLogin")
+//	public ModelAndView selectuserLogin() {
+//		return new ModelAndView("/selectuserLogin");
+//	}
+//	
+//	@GetMapping("/selectuserRegister")
+//	public ModelAndView selectuserRegister() {
+//		return new ModelAndView("/selectuserRegister");
+//	}
+	
+}
